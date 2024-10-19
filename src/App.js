98c258094154e4532c1eeeb44334d33d5370ca3e
Yuvan shankar <yuvanshankar@Yuvans-MacBook-Air-2.local>
2024-10-19 14:52:27 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        name: "John Doe",
+      },
+      counter: 1,
+      client: {
+        isLoading: false,
+        data: [],
+      },
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+      apiResponse: [...Array(4)],
+    });
+  };
+
+  handleFetch = () => {
+    this.setState({ client: { ...this.state.client, isLoading: true } });
+    setTimeout(() => {
+      this.setState({
+        client: { ...this.state.client, data: ["User 1", "User 2", "User 3"], isLoading: false, },
+      });
+    }, 3000);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.user.name}</h1>
+        <button onClick={this.handleClick}>Click - {this.state.counter}</button>
+        <button onClick={this.handleFetch}>Fetch</button>
+        <div>
+          {this.state.client.isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            this.state.client.data
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
